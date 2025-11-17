16a55de455c26af2010ca8f80a25353401537012
Stuart Lang <stuart.b.lang@gmail.com>
2025-11-17 10:52:08 +0000

    CS8620 C# 14 bug

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace CS8620_dotnet10_bug;
+
+public class Class1
+{
+    public void Test()
+    {
+        string s = "Values: ";
+        s = s.ConcatWith(MyEnum.Value1, MyEnum.Value2);
+    }
+}
+
+public static class StringExtensions
+{
+    extension(string str)
+    {
+        public string ConcatWith(params MyEnum[] enumValues)
+        {
+            return str + string.Join(",", enumValues);
+        }
+    }
+}
+
+public enum MyEnum
+{
+    Value1,
+    Value2
+}
